90a8e02c7b67b29e64204626714eabc0ad40715c
Nmrc <51129187+NeilMRC@users.noreply.github.com>
2023-02-17 13:20:46 +0000

    added Flask app

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 # This is the first file of the project 17/02/2023
+from flask import Flask
+
+app = Flask(__name__)
+print("from the app.")
+@app.route("/")
+def hello_world():
+  return "<p>Hello stinky world!</p>"
+
+
+if __name__=="__main__":
+  app.run(host='0.0.0.0',debug=True)
