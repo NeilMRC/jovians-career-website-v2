7f84244f95b617535a52d8d80707f353173af1b8
Nmrc <51129187+NeilMRC@users.noreply.github.com>
2023-02-20 10:54:38 +0000

    Added dynamic data and API  route

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
 # This is the first file of the project 17/02/2023
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
-print("from the app.")
+Company="Camel"
+JOBS=[
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location': 'Bangaluru, India',
+    'salary': 'Rs: 10,00,000'
+  },
+  {
+    'id':2,
+    'title':'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs: 15,00,000'
+  },
+  {
+    'id':3,
+    'title':'Frontend Engineer',
+    'location': 'Remote',
+    #'salary': 'Rs: 12,00,000'
+  },
+  {
+    'id':4,
+    'title':'Backend Engineer',
+    'location': 'Remote',
+    'salary': '$120,000.00'
+  },
+  
+]
+
+
+
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
-
+  return render_template("home.html",
+                        jobs=JOBS, company_name=Company)
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
+
