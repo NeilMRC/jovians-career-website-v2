e958a8c889ecc676c20df040fede7c343a27ff8c
Nmrc <51129187+NeilMRC@users.noreply.github.com>
2023-02-22 09:11:40 +0000

    Added connection DB

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # This is the first file of the project 17/02/2023
 # Modified to V2 on 21/02/2023
 from flask import Flask, render_template,jsonify
+from database import get_jobs_from_db
 
 app = Flask(__name__)
 Company="Camel"
@@ -33,14 +34,15 @@ JOBS=[
 ]
 
 
-
 @app.route("/")
 def hello_world():
+  jobs = get_jobs_from_db()
   return render_template("home.html",
-                        jobs=JOBS, company_name=Company)
+                        jobs=jobs, company_name=Company)
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs=get_jobs_from_db()
+  return jsonify(jobs)
 
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
---
+++ database.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine, text
+import os
+
+db_connection_string=os.environ['DB_CONNECTION_STRING']
+
+engine = create_engine(db_connection_string,
+                      connect_args={
+                        "ssl":{
+                          "ssl_ca":"/etc/ssl/cert.pem"
+                        }
+                      }
+                      )
+def get_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    result_all = result.all()
+    jobs=[]
+    for row in result_all:
+      jobs.append(row._asdict())
+    return jobs
+
+
+with engine.connect() as conn:
+  result = conn.execute(text("select * from jobs"))
+  result_all=result.all()
+  first_result=result_all[0]
+  first_result_dict=result_all[0]._asdict()
+ 
+
+result_dicts=[]
+for row in result_all:
+  result_dicts.append(row._asdict())
+print(result_dicts)
